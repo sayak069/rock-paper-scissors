4a65e667fca92fbb8eb7156a2ff57a3510b2fbb3
sayak069 <sayakbasak720@gmail.com>
2023-07-21 21:54:59 +0530

    added logic of the game

--- script.js
+++ script.js
@@ -9,4 +9,47 @@ function getComputerChoice(){
             return 'scissor';
     }
 }
-// console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection===computerSelection){
+        console.log('tied');
+        return -1;
+    }
+    if(playerSelection=='rock' && computerSelection=='scissor' 
+    || playerSelection=='scissor' && computerSelection=='paper'
+    || playerSelection=='paper' && computerSelection=='rock'){
+        console.log('You won!');
+        return 1;
+    }
+    if(playerSelection=='scissor' && computerSelection=='rock' 
+    || playerSelection=='paper' && computerSelection=='scissor'
+    || playerSelection=='rock' && computerSelection=='paper'){
+        console.log('You lose!');
+        return 0;
+    }
+}
+
+function game(){
+    let playerCount=0;
+    let computerCount=0;
+    for(let i=1;i<=5;i++){
+        let player=prompt("Enter your choice:").toLowerCase();
+        let computer=getComputerChoice();
+        console.log('Your choice: ' + player + ' ' + 'Computer choice: ' + computer);
+        let result=playRound(player,computer);
+        if(result==-1)  
+            continue;
+        if(result==1)
+            playerCount++;
+        if(result==0)
+            computerCount++;
+        console.log('Player: ' + playerCount + ' ' + 'Computer: ' + computerCount);
+    }
+    if(playerCount>computerCount)
+        console.log('You won the game');
+    else if(playerCount<computerCount)
+        console.log('You lose the game');
+    else
+        console.log('Game tied');
+}
+game();
