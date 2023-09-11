5ab52cae3f5701d8ddaa732b92204909ce447a58
sayak069 <sayakbasak720@gmail.com>
2023-09-11 23:12:39 +0530

    updated game functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function getComputerChoice(){
-    let choice=Math.floor(Math.random()*3);
-    switch(choice){
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
         case 0:
             return 'rock';
         case 1:
@@ -10,46 +10,52 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection===computerSelection){
-        console.log('tied');
-        return -1;
+let playerPts = 0
+let compPts = 0
+let para = document.getElementById('resPara')
+let pts = document.getElementById('pts')
+pts.textContent = `Player: ${playerPts} Computer: ${compPts}`
+function playRound() {
+    let computerSelection = getComputerChoice()
+    let playerSelection = this.value
+    if (playerSelection === computerSelection) {
+        para.textContent = 'Tied!'
     }
-    if(playerSelection=='rock' && computerSelection=='scissor' 
-    || playerSelection=='scissor' && computerSelection=='paper'
-    || playerSelection=='paper' && computerSelection=='rock'){
-        console.log('You won!');
-        return 1;
+    else if (playerSelection == 'rock' && computerSelection == 'scissor'
+        || playerSelection == 'scissor' && computerSelection == 'paper'
+        || playerSelection == 'paper' && computerSelection == 'rock') {
+        para.textContent = 'You won!'
+        playerPts++
     }
-    if(playerSelection=='scissor' && computerSelection=='rock' 
-    || playerSelection=='paper' && computerSelection=='scissor'
-    || playerSelection=='rock' && computerSelection=='paper'){
-        console.log('You lose!');
-        return 0;
+    else if (playerSelection == 'scissor' && computerSelection == 'rock'
+        || playerSelection == 'paper' && computerSelection == 'scissor'
+        || playerSelection == 'rock' && computerSelection == 'paper') {
+        para.textContent = `You lose!`
+        compPts++
+    }
+    para.textContent += ` Your choice: ${playerSelection} and Computer choice: ${computerSelection}`
+    pts.textContent = `Player: ${playerPts} Computer: ${compPts}`
+    winner()
+}
+// event firing
+const btn = document.querySelectorAll('.option')
+btn.forEach(ele => {
+    ele.addEventListener('click', playRound)
+})
+// decide winner
+function winner(){
+    if (playerPts == 5 || compPts == 5) {
+        btn.forEach(ele => {
+            ele.removeEventListener('click', playRound)
+        })
+        if (playerPts == 5)
+            para.textContent = "You won the game!"
+        else
+            para.textContent = "You lost the game to a computer!"
     }
 }
-
-function game(){
-    let playerCount=0;
-    let computerCount=0;
-    for(let i=1;i<=5;i++){
-        let player=prompt("Enter your choice:").toLowerCase();
-        let computer=getComputerChoice();
-        console.log('Your choice: ' + player + ' ' + 'Computer choice: ' + computer);
-        let result=playRound(player,computer);
-        if(result==-1)  
-            continue;
-        if(result==1)
-            playerCount++;
-        if(result==0)
-            computerCount++;
-        console.log('Player: ' + playerCount + ' ' + 'Computer: ' + computerCount);
-    }
-    if(playerCount>computerCount)
-        console.log('You won the game');
-    else if(playerCount<computerCount)
-        console.log('You lose the game');
-    else
-        console.log('Game tied');
-}
-game();
+// play again 
+const again = document.getElementById('again')
+again.addEventListener('click', () => {
+    location.reload()
+})
